61a02113bf60fba3d5f388adac03d67ac0c3db3e
Raghad74 <83588217+Raghad74@users.noreply.github.com>
2024-03-08 14:43:56 +0200

    initial commit

---
+++ Set.cpp
@@ -0,0 +1,18 @@
+#include "Set.h"
+
+int Set::getN()const
+{
+    return num;
+}
+int Set::getF()const
+{
+    return filling;
+}
+string Set::getC()const
+{
+    return color;
+}
+string Set::getS()const
+{
+    return shape;
+}
